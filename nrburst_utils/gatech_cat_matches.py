4367e3a24170898c33203fd54bafab496783eb98
James Clark <james.clark@ligo.org>
2015-11-17 02:53:55 -0500

    now using geometric units error series

--- nrburst_utils/gatech_cat_matches.py
+++ nrburst_utils/gatech_cat_matches.py
@@ -28,6 +28,7 @@ import cPickle as pickle
 
 import numpy as np
 import scipy.optimize
+from scipy import signal
 import timeit
 
 import lal
@@ -47,11 +48,71 @@ __author__ = "James Clark <james.clark@ligo.org>"
 gpsnow = subprocess.check_output(['lalapps_tconvert', 'now']).strip()
 __date__ = subprocess.check_output(['lalapps_tconvert', gpsnow]).strip()
 
-# Get the current git version
-#git_version_id = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
-#        cwd=os.path.dirname(sys.argv[0])).strip()
-#__version__ = "git id %s" % git_version_id
+def window_wave(input_data):
 
+    nonzero=np.argwhere(abs(input_data)>1e-3*max(abs(input_data)))
+    idx = range(nonzero[0],nonzero[-1])
+    win = planckwin(len(idx), 0.3)
+    win[0.5*len(win):] = 1.0
+    input_data[idx] *= win
+
+    return input_data
+
+def planckwin(N, epsilon):
+
+    t1 = -0.5*N
+    t2 = -0.5*N * (1.-2.*epsilon)
+    t3 = 0.5*N * (1.-2.*epsilon)
+    t4 = 0.5*N
+
+    Zp = lambda t: (t2-t1)/(t-t1) + (t2-t1)/(t-t2)
+    Zm = lambda t: (t3-t4)/(t-t3) + (t3-t4)/(t-t4)
+
+    win = np.zeros(N)
+    ts = np.arange(-0.5*N, 0.5*N)
+
+    for n,t in enumerate(ts):
+        if t<=t1:
+            win[n] = 0.0
+        elif t1<t<t2:
+            win[n] = 1./(np.exp(Zp(t))+1)
+        elif t2<=t<=t3:
+            win[n] = 1.0
+        elif t3<t<t4:
+            win[n] = 1./(np.exp(Zm(t))+1)
+
+    return win
+
+
+def scale_NR(times_codeunits, wave, mass, delta_t=1./2048):
+    """
+    Scale the waveform to total_mass.  Assumes the waveform is initially
+    generated at init_total_mass defined in this script.
+    """
+
+    peakidx = np.argmax(abs(wave))
+
+    NR_deltaT = np.diff(times_codeunits)[0]
+    NR_datalen = len(times_codeunits)
+    SI_deltaT_of_NR = mass * lal.MTSUN_SI * NR_deltaT
+
+    old_times = np.arange(0, NR_datalen*SI_deltaT_of_NR, SI_deltaT_of_NR)
+    interp_times = np.arange(0, NR_datalen*SI_deltaT_of_NR, delta_t)
+
+    resampled_wave = np.interp(interp_times, old_times, wave)
+
+    return pycbc.types.TimeSeries(resampled_wave, delta_t)
+
+def parse_NR_asc(file):
+    data = np.loadtxt(file)
+    times_codeunits     = np.loadtxt(file)[:,0]
+    hplus_NR_codeunits  = data[:,1]
+    hcross_NR_codeunits = data[:,2]
+    dAmpbyAmp_codeunits = data[:,3]
+    dphi_codeunits      = data[:,4]
+
+    return (times_codeunits, hplus_NR_codeunits, hcross_NR_codeunits,
+            dAmpbyAmp_codeunits, dphi_codeunits)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Parse input
@@ -61,12 +122,16 @@ __date__ = subprocess.check_output(['lalapps_tconvert', gpsnow]).strip()
 #
 bounds = dict()
 
+data_path='/home/jclark/Projects/numrel_bursts/gatech_data'
 # *************************************
 #   1)  D12_q2.00_a0.15_-0.60_m200 
-#    -  GATECH0199.h5
 bounds['q'] = [1.999, 2.001]
 bounds['spin1z'] = [0.14, 0.16]
 bounds['spin2z'] = [-0.59, -0.61]
+
+errors_file = os.path.join(data_path,
+        'Strain_Lframe_l2_m2_r75_D12_q2.00_a0.15_-0.60_m200.asc')
+
  
 #    2) Sq4_d9_a0.6_oth.270_rr_M180 
 #    - Highest resolution of this run missing in h5 catalog
@@ -76,19 +141,34 @@ bounds['spin2z'] = [-0.59, -0.61]
 #   bounds['spin2z'] = [0, 0.00001]
 #   bounds['spin1x'] = [-0.61, -0.59]
 #   bounds['spin2x'] = [-0.61, -0.59]
+#   errors_file = os.path.join(data_path,
+#           'Strain_Lframe_l2_m2_r75_Sq4_d9_a0.6_oth.270_rr_M180.asc')
+
 
 #    3) D7.5_q15.00_a0.0_CHgEEB_m800
-#    - missing from the h5 catalog
-#bounds['q'] = [14.99, 15.01]
+#   bounds['q'] = [14.99, 15.01]
+#   bounds['q'] = [10, np.inf]
+#   errors_file = os.path.join(data_path,
+#           'Strain_Lframe_l2_m2_r75_Sq4_d9_a0.6_oth.270_rr_M180.asc')
 
-#    4) RO3_D10_q1.50_a0.60_oth.090_M120
-#    - GATECH0173.h5
-#bounds['q'] = [1.4999, 1.50001]
-#bounds['spin1z'] = [0, 0.0001]
-#bounds['spin2z'] = [0.59, 0.61]
 
+#   #    4) RO3_D10_q1.50_a0.60_oth.090_M120
+#   bounds['q'] = [1.4999, 1.50001]
+#   bounds['spin1z'] = [0, 0.0001]
+#   bounds['spin2z'] = [0.59, 0.61]
+#   errors_file = os.path.join(data_path,
+#   'Strain_Lframe_l2_m2_r75_RO3_D10_q1.50_a0.60_oth.090_M120.asc')
+
+#    5) q8_LL_D9_a0.6_th1_45_th2_225
+#bounds['q'] = [7.5, 8.5]
+#errors_file = os.path.join(data_path,
+#'Strain_Lframe_l2_m2_r75_q8_LL_D9_a0.6_th1_45_th2_225_m400.asc')
+#'Strain_Simframe_l2_m2_r75_q8_LL_D9_a0.6_th1_45_th2_225_m400.asc')
 
 # *************************************
+
+times_codeunits, hplus_NR_codeunits, hcross_NR_codeunits, \
+        dAmpbyAmp_codeunits, dphi_codeunits = parse_NR_asc(errors_file)
 
 inc = 0 # inclination
 approx='SEOBNRv2'
@@ -111,19 +191,19 @@ maxMass = 500.0
 #
 # --- Time Series Config
 #
-delta_t = 1./2048
-datalen = 4
+delta_t = 1./4096
+datalen = 8
 
 #
 # --- Noise Spectrum
 #
 asd_file = \
-        "/home/jclark308/GW150914_data/noise_curves/early_aligo.dat"
+        "/home/jclark/GW150914_data/noise_curves/early_aligo.dat"
 
 #
 # --- Catalog
 #
-catalog='/home/jclark308/GW150914_data/nr_catalog/gatech_hdf5'
+catalog='/home/jclark/GW150914_data/nr_catalog/gatech_hdf5'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Generate The Catalogue
@@ -142,6 +222,9 @@ then = timeit.time.time()
 simulations = \
         nrbu.simulation_details(param_bounds=bounds,
                 catdir=catalog)
+
+if simulations.simulations[0]['wavefile'].split('/')[-1] ==  'GATECH1469.h5':
+    simulations.simulations[0]['Mmin30Hz'] = 71.0
 
 asd_data = np.loadtxt(asd_file)
 
@@ -183,6 +266,7 @@ for m,mass in enumerate(masses):
             simulations.simulations[0]['spin1z'],simulations.simulations[0]['spin2z'])
     ffinal = pnutils.get_final_freq(approx, mass1, mass2, 
             simulations.simulations[0]['spin1z'],simulations.simulations[0]['spin2z'])
+    #upp_bound = ffinal
     #upp_bound = 1.5*ffinal
     upp_bound = 0.5/delta_t
 
@@ -190,10 +274,20 @@ for m,mass in enumerate(masses):
     # NUMERICAL RELATIVITY
 
     # --- Generate the polarisations
-    hplus_NR, hcross_NR = nrbu.get_wf_pols(
-            simulations.simulations[0]['wavefile'], mass, inclination=inc,
-            delta_t=delta_t, f_lower=25 * min(masses) / mass, distance=distance)
+#   hplus_NR, hcross_NR = nrbu.get_wf_pols(
+#           simulations.simulations[0]['wavefile'], mass, inclination=inc,
+#           delta_t=delta_t, f_lower=29 * min(masses) / mass, distance=distance)
 
+    hplus_NR  = scale_NR(times_codeunits, hplus_NR_codeunits, mass,
+            delta_t=delta_t)
+    hcross_NR = scale_NR(times_codeunits, hcross_NR_codeunits, mass,
+            delta_t=delta_t)
+
+    hplus_NR.data = window_wave(hplus_NR.data)
+    hcross_NR.data = window_wave(hcross_NR.data)
+
+    hplus_NR = wfutils.taper_timeseries(hplus_NR, 'TAPER_STARTEND')
+    hcross_NR = wfutils.taper_timeseries(hcross_NR, 'TAPER_STARTEND')
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # APPROXIMANT 
@@ -201,53 +295,63 @@ for m,mass in enumerate(masses):
             distance=distance,
             mass1=mass1,
             mass2=mass2,
-            spin1x=simulations.simulations[0]['spin1x'],
-            spin2x=simulations.simulations[0]['spin2x'],
-            spin1y=simulations.simulations[0]['spin1y'],
-            spin2y=simulations.simulations[0]['spin2y'],
+            spin1x=0.0,
+            spin2x=0.0,
+            spin1y=0.0,
+            spin2y=0.0,
             spin1z=simulations.simulations[0]['spin1z'],
             spin2z=simulations.simulations[0]['spin2z'],
             inclination=inc,
             f_lower=f_low_approx * min(masses)/mass,
             delta_t=delta_t)
+            #spin1x=simulations.simulations[0]['spin1x'],
+            #spin2x=simulations.simulations[0]['spin2x'],
+            #spin1y=simulations.simulations[0]['spin1y'],
+            #spin2y=simulations.simulations[0]['spin2y'],
+            #spin1z=simulations.simulations[0]['spin1z'],
+            #spin2z=simulations.simulations[0]['spin2z'],
 
     hplus_approx = wfutils.taper_timeseries(hplus_approx, 'TAPER_START')
     hcross_approx = wfutils.taper_timeseries(hcross_approx, 'TAPER_START')
+
+
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # MATCH CALCULATION 
  
     # Make the timeseries consistent lengths
-    tlen = max(len(hplus_approx), datalen/delta_t)
+    tlen = max(len(hplus_approx), len(hplus_NR))
+    #tlen = int(datalen / delta_t)
+
     hplus_approx.resize(tlen)
     hplus_NR.resize(tlen)
     hcross_approx.resize(tlen)
     hcross_NR.resize(tlen)
-
     
     # ***********************
     # Errors
     # Convert to amplitude/phase
-
-    dAmpbyAmp = 0.01 # XXX Placeholder value
-    dphi = 0.25
-
-    amp_NR   = wfutils.amplitude_from_polarizations(hplus_NR, hcross_NR)
-    phi_NR = wfutils.phase_from_polarizations(hplus_NR, hcross_NR)
-
-    amp_NR_deltaUpp = amp_NR + dAmpbyAmp*amp_NR*np.random.random()
-    phi_NR_deltaUpp = phi_NR + dphi
-
+ 
+    dAmpbyAmp = scale_NR(times_codeunits, dAmpbyAmp_codeunits, mass, delta_t=delta_t)
+    dAmpbyAmp.resize(tlen)
+    dphi = scale_NR(times_codeunits, dphi_codeunits, mass, delta_t=delta_t)
+    dphi.resize(tlen)
+ 
+    amp_NR = wfutils.amplitude_from_polarizations(hplus_NR, hcross_NR)
+    amp_NR_deltaUpp = amp_NR + dAmpbyAmp*amp_NR
     amp_NR_deltaLow = amp_NR - dAmpbyAmp*amp_NR 
-    phi_NR_deltaLow = phi_NR - dphi
 
+    phi_NR = wfutils.phase_from_polarizations(hplus_NR, hcross_NR)
+    phi_NR_deltaUpp = phi_NR + dphi
+    phi_NR_deltaLow = phi_NR - dphi
+ 
     hplus_NR_deltaLow = \
             pycbc.types.TimeSeries(np.real(amp_NR_deltaLow*np.exp(1j*phi_NR_deltaLow)),
                     delta_t=hplus_NR.delta_t)
     hplus_NR_deltaUpp = \
             pycbc.types.TimeSeries(np.real(amp_NR_deltaUpp*np.exp(1j*phi_NR_deltaUpp)),
                     delta_t=hplus_NR.delta_t)
-
+ 
 
     # Interpolate the ASD to the waveform frequencies (this is convenient so that we
     # end up with a PSD which overs all frequencies for use in the match calculation
@@ -275,26 +379,27 @@ for m,mass in enumerate(masses):
             high_frequency_cutoff=upp_bound)
 
     mismatch = 100*(1-match)
+
     mismatch_deltaUpp = 100*(1-match_deltaUpp)
     mismatch_deltaLow = 100*(1-match_deltaLow)
+
+    mismatch_delta = abs(np.mean([mismatch_deltaUpp, mismatch_deltaLow]))
 
     # ------------------------------------------------------------------
     # DIAGNOSTIC PLOTS
 
     print "~~~~~~~~~~~~~~~~~~~~~~~"
     print "Mass: %.2f, mismatch: %.2f +/- %.2e (%%)"%(mass, mismatch,
-            abs(0.5*(mismatch_deltaUpp-mismatch_deltaLow)))
+            mismatch_delta)
 
     # Normalise to unit SNR
     snr_approx_100 = pycbc.filter.sigma(hplus_approx, psd=noise_psd,
             low_frequency_cutoff=30, high_frequency_cutoff=upp_bound)
     hplus_approx.data[:] /= snr_approx_100
-    print 'SNR of approximant @ %d Mpc=%.2f'%(distance,snr_approx_100)
 
     snr_NR_100 = pycbc.filter.sigma(hplus_NR,
             psd=noise_psd, low_frequency_cutoff=30, high_frequency_cutoff=upp_bound)
     hplus_NR.data[:] /= snr_NR_100
-    print 'SNR of NR @ %d Mpc=%.2f'%(distance,snr_NR_100)
 
     # Tdomain
 
@@ -308,14 +413,15 @@ for m,mass in enumerate(masses):
             hplus_approx.sample_times[maxidx], hplus_approx,
             label='approx')
 
-    ax[m][0].set_title('M$_{\mathrm{tot}}$=%.2f M$_{\odot}$, mismatch=%.2f %%'%(
-        mass, 100*(1-match)))
+    ax[m][0].set_title('M$_{\mathrm{tot}}$=%.2f M$_{\odot}$, mismatch=%.2f +/- %.2e (%%)'%(
+        mass, 100*(1-match), mismatch_delta))
 
     ax[m][0].set_xlabel('Time [s]')
     ax[m][0].set_ylabel('h(t) [arb units]')
 
 
-    ax[m][0].set_xlim(-0.25, 0.1)
+#    ax[m][0].set_xlim(-0.25, 0.1)
+    ax[m][0].set_xlim(-3, 1)
 
     # Fdomain
     Hplus_approx = hplus_approx.to_frequencyseries()
@@ -331,8 +437,8 @@ for m,mass in enumerate(masses):
     ax[m][1].loglog(noise_psd.sample_frequencies, np.sqrt(noise_psd),
             label='noise psd', color='k', linestyle='--')
 
-    ax[m][1].set_title('M$_{\mathrm{tot}}$=%.2f M$_{\odot}$, mismatch=%.2f %%'%(
-        mass, 100*(1-match)))
+    ax[m][1].set_title('M$_{\mathrm{tot}}$=%.2f M$_{\odot}$, mismatch=%.2f +/- %.2e (%%)'%(
+        mass, 100*(1-match), mismatch_delta))
 
     ax[m][1].legend(loc='upper right')
 
