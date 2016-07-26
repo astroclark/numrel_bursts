4a8bfb955ea2a2f8b5ba7e5bef93f3f9f87b629f
James Clark <james.clarrk@ligo.org>
2016-07-26 13:06:57 -0400

    labels

--- nrburst_utils/nrburst_bwcomp2pops.py
+++ nrburst_utils/nrburst_bwcomp2pops.py
@@ -62,26 +62,64 @@ for i,injfile in enumerate(injfiles):
 # --- Overlap distributions
 f, ax = pl.subplots()
 medianoverlaps = []
+filename=[]
+cols=['black', 'red']
 for i in xrange(len(injfiles)):
+
     medianoverlaps.append(np.median(results[i]['mynetoverlaps'], axis=1))
-    ax.hist(medianoverlaps[i], normed=True, histtype='step', cumulative=True,
-            bins=100)
+
+    label=injfiles[i].replace('HL-INJECTIONS-','')
+    label=label.replace('pseudoFourPN','')
+    label=label.replace('.npz','')
+#    label=label+' (a|b)=%.2f'%np.median(medianoverlaps[i])
+
+    print injfiles[i]
+    print label
+    filename.append(label)
+
+    wave=injfiles[i].split('-')[2].replace('pseudoFourPN','')
+    massratio = injfiles[i].split('-')[3].replace('q_','')
+    inclination = injfiles[i].split('-')[4].replace('iota_','')
+    fmin = injfiles[i].split('-')[-1].replace('.npz','').replace('fmin_','')
+
+    titlabel="netsnr=%f, q=%s, inclination=%s"%(netsnr, massratio, inclination)
+
+    medianmedianoverlaps=np.median(medianoverlaps[i])
+    stdmedianoverlaps=np.std(medianoverlaps[i])
+    
+    h=ax.hist(medianoverlaps[i], normed=True, histtype='step', cumulative=True,
+            bins=100, label=wave+' (a|b)=%.2f+/-%.2f [fmin=%s]'%(
+                medianmedianoverlaps, stdmedianoverlaps, fmin), color=cols[i])
+    ax.axvline(np.median(medianoverlaps[i]), color=cols[i])
+
+    # Dump out characteristics so we can plot as function of SNR
+    f=open(filename[i]+'.dat', 'w')
+    f.writelines("#fmin q iota netsnr medianoverlap stdmedianoverlap\n")
+    f.writelines("%s %s %s %f %f %f\n"%(fmin, massratio, inclination,
+        netsnr, medianmedianoverlaps, stdmedianoverlaps))
+    f.close()
+
+ax.legend(loc='lower right')
+filename=filename[0]+'-VS-'+filename[1]
 
 # Get p-value for KS test between first two distributions
-if len(injfiles)==2:
-    from scipy import stats
-    _, p=stats.ks_2samp(medianoverlaps[0],medianoverlaps[1])
-    ax.set_title('KS-test p-value: %.2f'%p)
-else:
-    ax.set_title('Median overlaps across injection populations')
+from scipy import stats
+_, p=stats.ks_2samp(medianoverlaps[0],medianoverlaps[1])
+ax.set_title(titlabel+' [KS-test p-value: %.2f]'%p)
 
 ax.axhline(0.5, color='k', linewidth=2, linestyle='--', label='Median overlap')
 ax.minorticks_on()
 ax.set_ylim(0,1)
+ax.set_xlim(0.85, 1)
 ax.set_xlabel('Median Network overlap')
 ax.set_ylabel('CDF [over injection population]')
 
-pl.show()
+pl.savefig(filename)
+#pl.show()
+
+
+
+
 
 # Save command for reduced moments:
 
